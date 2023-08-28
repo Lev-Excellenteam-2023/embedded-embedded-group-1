13e888d0015091faf800b59864d2d4ba3594bef6
mendy k <mendikahana@gmail.com>
2023-08-28 14:33:04 +0300

    exception camera disconnected

--- detector/cam.py
+++ detector/cam.py
@@ -5,9 +5,11 @@ import time
 def camra():
     camera = cv2.VideoCapture(0)
     return_value, image = camera.read()
+    if (not return_value):
+        raise Exception("camera disconected")
     return image
-"""def main():
+def main():
     m=camra()
     cv2.imwrite('m'+ '.png', m)
 if __name__ == "__main__":
-    main()"""
+    main()
