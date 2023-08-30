1c982c28fe046eadfad07ff50d234aaa4143a7ce
Daniel Bodenstein <danielboden111@gmail.com>
2023-08-30 10:39:33 +0300

    fix utils_for_data

--- db/utils_for_data.py
+++ db/utils_for_data.py
@@ -5,15 +5,6 @@ import logging
 from db import config
 
 
-'''
-template of json file:
-birds_data = {
- "camera_id" = 123
- "time" = datetime
- "num_of_birds" = 10
- }
-'''
-
 def store_data(data_birds: dict) -> None:
     """
     The function get a dictionary include data about birds, and save it into a json file
@@ -41,7 +32,7 @@ def get_data(since: datetime, to: datetime) -> list[dict]:
         birds_list = json.load(json_file)
         filtered_data = []
         for birds_data in birds_list:
-            bird_datetime = datetime.datetime.fromisoformat(birds_data["time"])
+            bird_datetime = datetime.datetime.strptime(birds_data["time"], "%d/%m/%Y %H:%M:%S")
             if since <= bird_datetime <= to:
                 filtered_data.append(birds_data)
         return filtered_data
